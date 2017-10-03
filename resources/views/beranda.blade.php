78245466488ebce6e9d0bba94c6b52dbea548b4f
drmifta <miftadrachma@gmail.com>
2017-10-03 02:45:05 -0700

    buat halaman beasiswa

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -98,7 +98,7 @@
                         Beasiswa PPA 2017 dan BPP PPA 2017 ditujukan bagi mahasiswa aktif yang berada di jenjang S1/DIV atau mahasiswa D3. Ketentuan lebih lanjut tertera di persyaratan.</p>
                     <img width="100%" src={{("image/ppa.jpg")}} alt="">
                     <br><br>
-                    <a href="http://materializecss.com/themes.html" class="btn-large waves-effect waves-light" style="background-color: #03a9f4">Baca</a>
+                    <a href="beasiswa" class="btn-large waves-effect waves-light" style="background-color: #2F4F4F">Baca</a>
                 </div>
                 <div class="col s6 ">
                     <h5 class="light header">Beasiswa BCA</h5>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('beranda');
 });
+Route::get('/beasiswa', function () {
+    return view('beasiswa');
+});
+
